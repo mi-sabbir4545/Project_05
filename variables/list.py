dc9be85c5ec06d8c89ecacfc1c7ef3059ff31ce5
mi-sabbir <sabbir722722@gmail.com>
2022-08-27 12:59:44 +0600

    dictonary

---
+++ variables/list.py
@@ -0,0 +1,51 @@
+
+#student1 = ['David', 'mail@gmail.com', '12345678', 'Ny']
+#student2 = ['Munsur', 'munsur@gmail.com', '12345678', 'DhAKA']
+
+#print(student1[0])
+#print(len(student1))
+
+#student2_name = student2[0]
+#print(student2_name)
+
+#guest = ['David', 'munsur', 'lekhon', 'minhaj', 'sujit', 'mahbub', 'asif', 'ruman', 'toukir', 'hridoy']
+
+#lunch = ['David', 'munsur', 'lekhon', 'minhaj', 'sujit']
+#dinner = ['mahbub', 'asif', 'ruman', 'toukir', 'hridoy']
+
+#print(guest)
+
+#lunch_guest = [[guest[0], guest[2], guest[3], guest[4], guest[4]]
+
+#lunch_guest = guest[0:5]
+
+#dinner_guest = guest[5:]
+#special_guest = guest[2]
+
+#print(lunch_guest)
+#print(dinner_guest)
+
+number = [12,23,3,42,23,3,44]
+#number.sort()
+#print(number)
+
+#number.insert(3,10)
+#print(number)
+
+#number.remove(23)
+#print(number)
+
+#number.reverse()
+#print(number)
+
+#number.append(100)
+#print(number)
+
+number.clear()
+print(number)
+
+
+
+
+
+
